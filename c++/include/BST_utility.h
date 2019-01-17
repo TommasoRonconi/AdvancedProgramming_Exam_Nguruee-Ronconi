1905d509092ab17e5a4bdd8d7cc2dc59f3de028b
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-01-17 10:41:48 +0100

    minors in BST_utility.h

--- c++/include/BST_utility.h
+++ c++/include/BST_utility.h
@@ -1,43 +1,49 @@
 /**
- *  @file include/Tree.h
+ *  @file include/BST_utility.h
  *
- *  @brief The class Tree
+ *  @brief Header containing declaration of some helper functions and types
  *
- *  This file defines the interface of the class Tree
+ *  This file declares some helper functions and custom-types
  *
  *  @author Herbert Nguruwe, Tommaso Ronconi
  *
  *  @author hknguruwe@gmail.com, tronconi@sissa.it
  */
+#ifndef __BST_UTILITY__
+#define __BST_UTILITY__
+
 #include<iostream>
 #include<memory>
+
 /**                                                                               
-   *  @brief The namespace of the functions and classes used to handle 
-   *  <B> catalogues of astronomical sources </B>   
-   *                                                       
-   *  The \e catalogue namespace contains all the functions and                              
-   *  classes used to handle catalogues of astronomical sources                                   
-*/
+ *  @brief The namespace of the functions and classes used to handle 
+ *  <B> catalogues of astronomical sources </B>   
+ *                                                       
+ *  The \e catalogue namespace contains all the functions and                              
+ *  classes used to handle catalogues of astronomical sources                                   
+ */
 namespace bst{
 
-/**
- * @enum direction
- * @brief Direction is used to decide where to add the node. Its either right or left
-*/
-enum class direction{left, right};
+  /**
+   * @enum direction
+   * @brief Direction is used to decide where to add the node. Its either right or left
+   */
+  enum class direction{left, right};
 
-/**
- *  @brief get the pointer corresponding to bst::direction
- *  
- *  @param dir the direction to take
- *
- *  @return pointer to child in the given direction
- */
-template<class T> 
-direction which_direction(const T keychild, const T keyparent)
-{
-    if (keychild > keyparent) return direction::right;
-    else return direction::left;
+  /**
+   *  @brief get the pointer corresponding to bst::direction
+   *  
+   *  @param dir the direction to take
+   *
+   *  @return pointer to child in the given direction
+   */
+  template<class T> 
+    direction which_direction(const T keychild, const T keyparent)
+    {
+      if (keychild > keyparent) return direction::right;
+      else return direction::left;
+    }
+
 }
 
-}
+#endif //__BST_UTILITY__
