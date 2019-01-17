966c6b0c56e7986471e17eeb5dcb8b2f07dd0cce
hibozzy18 <hknguruwe@gmail.com>
2019-01-17 15:25:39 +0100

    add insert function

--- c++/src/Tree.cc
+++ c++/src/Tree.cc
@@ -1,5 +1,7 @@
 #include "../include/Tree.h"
 #include <iostream>
+#include<memory>
+#include"../include/BST_utility.h"
 
 // ===========================================================================
 
@@ -11,6 +13,32 @@
 
 // }
 
+void Tree::m_insert(std::unique_ptr<Tree::Node> &newnode, Iterator it,const bool substitute){
+     bst::direction dir = bst::which_direction(newnode->key,it->key );
+     
+     if (it->get_direction(dir)) {
+         m_insert(newnode, Iterator(it->get_direction(dir)), substitute);
+     }
+     else {
+         it->new_node(newnode, dir);
+     }
+     
+ 
+}
 
+template <class T, class U>
+void Tree::insert(const T key, const U value, const bool substitute)
+{
+    Node newNode {key, value};
+    
+    if (root) {
+       
+         m_insert(newNode,top(), true);
+    }
+    else {
+        
+        root = newnode;
+    }
+    
 
-
+}
