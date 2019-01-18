69746d62d79c1c7d4f8634478a972b8f332b9c7e
hibozzy18 <hknguruwe@gmail.com>
2019-01-18 22:18:53 +0100

    add new insert in tree

--- c++/src/Tree.cc
+++ c++/src/Tree.cc
@@ -2,7 +2,27 @@
 
 
 // ===========================================================================
+/**
+   *  @brief get the pointer corresponding 
+   *  
+   *  @param dir the direction to take
+   *
+   *  @return pointer to child in the given direction
+   */
 
+void Tree::insert ( const int k, const int v, bool sub ){
+
+  if (root)
+
+    root->insert( k, v, sub );
+
+  else{
+  
+    root.reset( new Node{ k, v } );
+
+  }
+
+}
 
 
 
