1b8f476b6d6c008673ff5fa22af8be2352b3be2e
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-01-23 09:42:13 +0100

    minor fixed in plot script

--- c++/analysis/plot_results.py
+++ c++/analysis/plot_results.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-filein = "../output/test_unordered.dat"
+filein = "../output/test_ordered.dat"
 
 ss, minTu, Tu, maxTu, minTo, To, maxTo, minTm, Tm, maxTm = np.genfromtxt(filein, unpack=True)
 
 fig = plt.figure( figsize=(12,8) )
 ax = fig.add_subplot( 1, 1, 1 )
 
-ax.set_title("Find $42$ (random input)", fontsize=22)
+ax.set_title("Find $42$ (ordered input)", fontsize=22)
 ax.set_xscale('log')
 ax.set_xlim([70, 1.4e+5])
 ax.set_xlabel("$N_\\mathrm{nodes}$", fontsize=18)
@@ -20,7 +20,7 @@ ax.set_ylabel("time for the answer [nsec]", fontsize=18)
 ax.set_yticks([100., 1000.])
 ax.set_yticklabels(["100", "1000"], fontsize=18)
 
-ax.plot(ss, To[0]*np.log10(ss)/np.log10(ss[0]), '--k', alpha=0.7, label="$N \\propto\\ \\log N$")
+ax.plot(ss, To[0]*np.log10(ss)/np.log10(ss[0]), '--k', alpha=0.7, label="$t \\propto\\ \\log N$")
 ax.plot(ss, Tu, lw=2., label="Tree unbalanced")
 ax.fill_between(ss, minTu, maxTu, alpha=0.4)
 ax.plot(ss, To, lw=2., label="Tree balanced")
@@ -28,5 +28,5 @@ ax.fill_between(ss, minTo, maxTo, alpha=0.4)
 ax.plot(ss, Tm, lw=2., label="std::map")
 ax.fill_between(ss, minTm, maxTm, alpha=0.4)
 ax.legend(loc='lower right', fontsize=18, ncol=2)
-plt.savefig("../output/plot_random.png", bbox_inches='tight')
+plt.savefig("../output/plot_ordered.png", bbox_inches='tight')
 plt.show()
