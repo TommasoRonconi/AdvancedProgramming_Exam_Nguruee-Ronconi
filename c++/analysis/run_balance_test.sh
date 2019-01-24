2c5d7f0b2a83e906d8f9ba584b8fb3d5893b7ac7
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-01-24 22:27:25 +0100

    modified function to test performance and added function to test balance time (in main.c) added script for testing balance (in analysis) and data for testing perfomance (in input)

--- c++/analysis/run_balance_test.sh
+++ c++/analysis/run_balance_test.sh
@@ -7,12 +7,8 @@ mkdir -p ${output}
 
 cd ${workdir}
 make clean
-make OPT+=-DPERFORMANCE
+make OPT+=-DTEST_BALANCE
 cd ${tools}
-
-function random_input {
-    ${tools}/random.sh -m 100000 -n $(( 2*$1 )) | awk 'BEGIN{tmp=0;} { if (FNR%2 != 0) tmp = $1; else print int(tmp), $1/1000. }'
-}
 
 function ordered_input {
     ${tools}/random.sh -m 100000 -n $1 | awk ' { print FNR, $1/1000. }'
@@ -24,22 +20,10 @@ function ordered_input {
 	{
 	    for ii in {1..10}
 	    do
-		random_input $size | ${workdir}/tree.x
-	    done
-	} | awk -v run="$size" -f ${tools}/avg_min_max.awk
-    done
-} > ${output}/test_unordered.dat
-
-{
-    for size in 100 500 1000 5000 10000 15000 20000 25000 30000
-    do
-	{
-	    for ii in {1..10}
-	    do
 		ordered_input $size | ${workdir}/tree.x
 	    done
 	} | awk -v run="$size" -f ${tools}/avg_min_max.awk
     done
-} > ${output}/test_ordered.dat
+} > ${output}/test_balance.dat
 
     
