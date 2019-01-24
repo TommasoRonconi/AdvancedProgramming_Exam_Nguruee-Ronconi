255115efccd2dd9774d10a31900232f7fd719125
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2019-01-25 00:19:30 +0100

    updated plot performance

--- c++/analysis/plot_results.py
+++ c++/analysis/plot_results.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-filein = "../output/test_unordered.dat"
+filein = "../output/test_ordered.dat"
 
 ss, minTu, Tu, maxTu, minTo, To, maxTo, minTm, Tm, maxTm = np.genfromtxt(filein, unpack=True)
 
 fig = plt.figure( figsize=(12,8) )
 ax = fig.add_subplot( 1, 1, 1 )
 
-ax.set_title("find() (random input)", fontsize=22)
+ax.set_title("find() (ordered input)", fontsize=22)
 ax.tick_params(axis='both', labelsize=18)
 ax.set_xscale('log')
 # ax.set_xlim([70, 1.4e+5])
@@ -29,5 +29,5 @@ ax.fill_between(ss, minTo, maxTo, alpha=0.4)
 ax.plot(ss, Tm, lw=2., label="std::map")
 ax.fill_between(ss, minTm, maxTm, alpha=0.4)
 ax.legend(loc='upper left', fontsize=18, ncol=2)
-plt.savefig("../output/plot_random.png", bbox_inches='tight')
+plt.savefig("../output/plot_ordered.png", bbox_inches='tight')
 plt.show()
